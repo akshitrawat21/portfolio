d5539baa8ba00ea84f5415bb9d21ff7dd6732a32
akshitrawat21 <rawatakshit1234@gmail.com>
2024-08-23 12:50:35 +0530

    footer updaate

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,12 +7,6 @@ export default function Footer() {
       <small className="mb-2 block text-xs">
         &copy; 2024 Akshit. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
-      
       {/* Social Media Links */}
       <div className="mt-4 flex justify-center gap-4">
         <a
